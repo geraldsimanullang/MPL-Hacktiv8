e7ca0bf73f8165e084d08a55dd9abf15174dd0d9
Jungun Sitompul <jungunsitompul13@gmail.com>
2024-08-15 21:18:04 +0700

    UP/2/Login

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000
 const router = require('./routers')
 
 app.set('view engine', 'ejs')
-
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}))
 app.use(session({
   secret: 'you know when you know it',
